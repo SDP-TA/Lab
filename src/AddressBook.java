81189f07d78b7cbc49e8fe23ee2de8ea15e6dc34
Haoyuan <tmpt@mail.com>
2020-09-24 02:55:59 -0400

    feat: add and remove a buddy from an address book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,17 @@
 public class AddressBook {
+
     public static void main(String[] args) {
-        System.out.print("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        // to be finished
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        // to be finished
     }
 }
