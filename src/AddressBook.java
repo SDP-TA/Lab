24fab94c67fdc450bdf9caf97f95c0df0042795e
Haoyuan <tmpt@mail.com>
2020-10-01 22:49:54 -0400

    fix: modify the removeBuddy function

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,17 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
+
+    private List<BuddyInfo> myBuddies = new ArrayList<>();
 
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
     }
 
     public void addBuddy(BuddyInfo buddy) {
-        // to be finished
+        if(buddy != null)
+            myBuddies.add(buddy);
     }
 
-    public void removeBuddy(BuddyInfo buddy) {
-        // to be finished
+    public BuddyInfo removeBuddy(int index) {
+        if(index >= 0 && index < myBuddies.size())
+            return myBuddies.remove(index);
+        else
+            return null;
     }
 }
